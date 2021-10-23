8d39dc1f362840b9810635ee8933605ef3cfb98c
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-10-23 10:55:06 +0530

    CabInvoice code

---
+++ src/main/java/com/bl/Ride.java
@@ -0,0 +1,13 @@
+package com.bl;
+
+public class Ride {
+    public final double distance;
+    public final int time;
+    public final RideType rideType;
+
+    public Ride(RideType rideType, double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+        this.rideType = rideType;
+    }
+}
